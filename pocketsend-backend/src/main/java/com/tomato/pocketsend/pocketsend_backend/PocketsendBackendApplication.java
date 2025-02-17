c0e9ad321fb326d632795d7700cbc342bb46a696
Qianqian Zhou <qqzh1214@gmail.com>
2025-02-17 18:34:10 +0100

    Setup frontend files

---
+++ pocketsend-backend/src/main/java/com/tomato/pocketsend/pocketsend_backend/PocketsendBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tomato.pocketsend.pocketsend_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class PocketsendBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocketsendBackendApplication.class, args);
+	}
+}
---
+++ pocketsend-backend/src/test/java/com/tomato/pocketsend/pocketsend_backend/PocketsendBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomato.pocketsend.pocketsend_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocketsendBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
