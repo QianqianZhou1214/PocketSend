f706d5323e4f278ce45f49b2606abb0f345fddaf
Qianqian Zhou <qqzh1214@gmail.com>
2025-02-21 13:29:21 +0100

    Add: SideBar

---
+++ pocketsend-frontend/src/components/NavBar.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { useNavigation } from "react-router-dom";
+import logo from "../assets/pocketsendlogo.png";
+
+export default function Navbar() {
+ 
+}
---
+++ pocketsend-frontend/src/components/SideBar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function SideBar({ isOpen, toggleSideBar }) {
+  const navigate = useNavigate();
+
+  return (
+    <div className={`sidebar ${isOpen ? "open" : ""}`}>
+      <button className="close-btn" onClick={toggleSideBar}>
+        X
+      </button>
+      <ul>
+        <li onClick={() => { navigate("/"); toggleSideBar(); }}>Home</li>
+        <li onClick={() => { navigate("/profile"); toggleSideBar(); }}>Account</li>
+        <li onClick={() => { navigate("/logout"); toggleSideBar(); }}>Logout</li>
+      </ul>
+    </div>
+  );
+}
