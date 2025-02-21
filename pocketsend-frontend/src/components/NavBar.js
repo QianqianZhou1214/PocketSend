f7419b2de9df3992c4324f90eddb0216766a58c9
Qianqian Zhou <qqzh1214@gmail.com>
2025-02-21 16:04:53 +0100

    add: NavBar and styles

--- pocketsend-frontend/src/components/NavBar.js
+++ pocketsend-frontend/src/components/NavBar.js
@@ -1,7 +1,24 @@
-import React from "react";
-import { useNavigation } from "react-router-dom";
-import logo from "../assets/pocketsendlogo.png";
+import React, { useState } from "react";
+import SideBar from "./SideBar";
+import logo from "../assets/pocketsendlogo.jpg";
+import "./styles/NavBar.css";
 
 export default function Navbar() {
+  const [isSideBarOpen, setIsSideBarOpen] = useState(false);
+
+  const toggleSideBar = () => {
+    setIsSideBarOpen(!isSideBarOpen);
+  };
+
+  return (
+    <>
+      <nav className="navbar">
+        <div className="logo" onClick={toggleSideBar}>
+          <img src={logo} alt="Logo" />
+        </div>
+      </nav>
+      <SideBar isOpen={isSideBarOpen} toggleSideBar={toggleSideBar} />
+    </>
+  );
  
 }
--- pocketsend-frontend/src/components/SideBar.js
+++ pocketsend-frontend/src/components/SideBar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import "./styles/SideBar.css";
 
 export default function SideBar({ isOpen, toggleSideBar }) {
   const navigate = useNavigate();
