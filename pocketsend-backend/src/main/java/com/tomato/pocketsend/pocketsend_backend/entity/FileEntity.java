08f921fa941caac6f811c18fe827230361911205
Qianqian Zhou <qqzh1214@gmail.com>
2025-04-02 16:20:39 +0200

    Backend add: User Entity update: FileEntity add: User Repository

--- pocketsend-backend/src/main/java/com/tomato/pocketsend/pocketsend_backend/entity/FileEntity.java
+++ pocketsend-backend/src/main/java/com/tomato/pocketsend/pocketsend_backend/entity/FileEntity.java
@@ -28,6 +28,10 @@ public class FileEntity {
     @Transient
     private String url;
 
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
 
     public FileEntity() {
     }
