78e6c3c67c9d31bbee7d40ced5fa67ff8cdc1687
Qianqian Zhou <qqzh1214@gmail.com>
2025-04-06 17:49:11 +0200

    Updated CSS format for Register

--- pocketsend-frontend/src/pages/Register.js
+++ pocketsend-frontend/src/pages/Register.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import './styles/Register.css';
 
 export default function Register() {
   const [form, setForm] = useState(
