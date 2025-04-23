e3e3be7fbbff2d20338471fcf8546065a70ec83d
Qianqian Zhou <qqzh1214@gmail.com>
2025-04-23 17:55:21 +0200

    Revised id to UUID

--- pocketsend-backend/src/main/java/com/tomato/pocketsend/pocketsend_backend/repositories/FileRepository.java
+++ pocketsend-backend/src/main/java/com/tomato/pocketsend/pocketsend_backend/repositories/FileRepository.java
@@ -1,13 +1,14 @@
 package com.tomato.pocketsend.pocketsend_backend.repositories;
 
-import com.tomato.pocketsend.pocketsend_backend.entity.FileEntity;
+import com.tomato.pocketsend.pocketsend_backend.entity.File;
 import com.tomato.pocketsend.pocketsend_backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.UUID;
 
 
-public interface FileRepository extends JpaRepository<FileEntity, Long> {
-    List<FileEntity> findByOwner(User owner);
+public interface FileRepository extends JpaRepository<File, UUID> {
+    List<File> findByOwner(User owner);
 
 }
