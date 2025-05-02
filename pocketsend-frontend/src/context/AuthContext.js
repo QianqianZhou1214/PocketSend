31c05f4d1c3bb0dc17c44986819a4f4912c12f9b
Qianqian Zhou <qqzh1214@gmail.com>
2025-05-02 17:03:09 +0200

    Added： fetchCurrentUser updateUser

--- pocketsend-frontend/src/context/AuthContext.js
+++ pocketsend-frontend/src/context/AuthContext.js
@@ -1,8 +1,9 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
   const [isAuthenticated, setIsAuthenticated] = useState(
     !!localStorage.getItem("accessToken")
   );
@@ -32,17 +33,72 @@ export const AuthProvider = ({ children }) => {
     }
   };
 
-  const logout = async () => {
+  const logout = () => {
+    localStorage.removeItem("accessToken");
+    setUser(null);
+  };
+
+  /*const logout = async () => {
     await fetch("http://localhost:8080/api/auth/logout", {
       method: "POST",
       credentials: "include"
     });
     localStorage.removeItem("accessToken");
     setIsAuthenticated(false);
+  };*/
+
+  const fetchCurrentUser = async () => {
+    try {
+      const res = await fetch("http://localhost:8080/api/auth/profile", {
+        credentials: "include",
+        headers: {
+          "Authorization": `Bearer ${localStorage.getItem("accessToken")}`
+        }
+      });
+
+      if(res.ok) {
+        const data = await res.json();
+       setUser(data);
+      } else {
+        console.error("Failed to fetch user info.");
+      }
+    } catch (err) {
+      console.error("Error fetching info", err);
+    }
+  };
+
+  useEffect(() => {
+    const token = localStorage.getItem("accessToken");
+    if (token) {
+      fetchCurrentUser();
+    }
+  }, []);
+
+  const updateUser = async (updatedData) => {
+    try {
+      const response = await fetch("http://localhost:8080/api/auth/profile", {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${localStorage.getItem("accessToken")}`,
+        },
+        credentials: "include",
+        body: JSON.stringify(updatedData),
+      });
+  
+      if (response.ok) {
+        const updatedUser = await response.json();
+        setUser(updatedUser);
+      } else {
+        console.error("Failed to update user");
+      }
+    } catch (error) {
+      console.error("Error updating user", error);
+    }
   };
 
   return (
-    <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
+    <AuthContext.Provider value={{ isAuthenticated, login, logout, user, updateUser }}>
       {children}
     </AuthContext.Provider>
   );
